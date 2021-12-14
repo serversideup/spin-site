658ae21ba88285abe45df99f9ff1225ad61016cc
Jay Rogers <jay@521dimensions.com>
2021-12-14 11:37:37 -0600

    Added data

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,23 @@
 import theme from '@nuxt/content-theme-docs'
 
+const createSitemapRoutes = async () => {
+  const { $content } = require('@nuxt/content')
+  let routes = [];
+
+  const posts = await $content({ deep: true })
+    .only(['to'])
+    .fetch()
+
+  posts.forEach( function( post, index ){
+    // For some reason it always pulls the settings.json from the content.
+    if( post.to != '//settings' ){
+      routes.push( post.to );
+    }
+  });
+
+  return routes;
+}
+
 export default theme({
   docs: {
     primaryColor: '#3B82F6'
@@ -17,10 +35,10 @@ export default theme({
   },
   router: {
     base: '/open-source/spin/'
-  }
+  },
   sitemap: {
     hostname: 'https://serversideup.net',
     gzip: true,
     routes: createSitemapRoutes
-  },
+  }
 })
